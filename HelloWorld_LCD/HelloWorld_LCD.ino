58b922a9d78d51f7c885ca704490aea168bb04bd
Jason Altekruse <altekrusejason@gmail.com>
2023-05-07 19:40:49 -0500

    reading controller input
    - integrating code from this example
    - https://www.instructables.com/Connect-and-Use-Joystick-With-Arduino/
    - need to still delete stuff for sending signals to the LEDs that
      I am not using

--- HelloWorld_LCD/HelloWorld_LCD.ino
+++ HelloWorld_LCD/HelloWorld_LCD.ino
@@ -502,6 +502,30 @@ byte run3[] = {
   B00010
 };
 
+
+// handle joystick input
+
+int UD = 0;
+int LR = 0;
+/* Arduino Micro output pins*/
+
+int DWN = 13;
+int UP = 12;
+int LEFT = 11;
+int RT = 10;
+
+/*Arduino Micro Input Pins */
+
+int IUP=A0;
+int ILR=A1;
+int MID = 10; // 10 mid point delta arduino, use 4 for attiny
+int LRMID = 0;
+int UPMID = 0;
+
+int x_pos = 0;
+int y_pos = 0;
+
+
 void setup() {
   lcd.begin(16, 2);
   lcd.clear();
@@ -509,14 +533,68 @@ void setup() {
   lcd.createChar(1, run1);
   lcd.createChar(2, run2);
   lcd.createChar(3, run3);
+
+  // controller input
+  // TODO - might need to delete some as this was also driving LEDs to show direction
+  pinMode(DWN, OUTPUT);
+  pinMode(UP, OUTPUT);
+  pinMode(LEFT, OUTPUT);
+  pinMode(RT, OUTPUT);
+
+  digitalWrite(DWN, HIGH);
+  digitalWrite(UP, HIGH);
+  digitalWrite(LEFT, HIGH);
+  digitalWrite(RT, HIGH);
+
+  //calabrate center
+  LRMID = analogRead(ILR);
+  UPMID = analogRead(IUP);
 }
 
 void loop() {
   while (1) {
     for (int i = 0; i < 4; i++) {
-      lcd.setCursor(0, 0);
+      lcd.setCursor(x_pos, y_pos);
       lcd.write((char)i);
-      delay(200);
+
+      UD = analogRead(IUP);
+      LR = analogRead(ILR);
+
+      // UP-DOWN
+
+      if(UD < UPMID - MID){
+        x_pos = x_pos == 0 ? 0 : x_pos-1;
+        lcd.clear();
+        // digitalWrite(DWN, HIGH);
+      }else{
+        digitalWrite(DWN, LOW);
+      }
+
+      if(UD > UPMID + MID) {
+        x_pos = x_pos == 15 ? 15 : x_pos+1;
+        lcd.clear();
+        //digitalWrite(UP, HIGH);
+      }else{
+        digitalWrite(UP, LOW);
+      }
+      
+      // LEFT-RIGHT
+      if(LR < LRMID - MID){
+        y_pos = y_pos == 0 ? 0 : y_pos-1;
+        lcd.clear();
+        //digitalWrite(LEFT, HIGH);
+      }else{
+        digitalWrite(LEFT, LOW);
+      }
+
+      if(LR > LRMID + MID){
+        y_pos = y_pos == 1 ? 1 : y_pos+1;
+        lcd.clear();
+        //digitalWrite(RT, HIGH);
+      }else{
+        digitalWrite(RT, LOW);
+      }
+      delay(100);
     }
   }
 }
